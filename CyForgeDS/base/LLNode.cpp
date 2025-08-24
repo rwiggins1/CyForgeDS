4584ca8813726bbcac3fc4de60e8032c12ae0be4
rwiggins1 <wigginsricky94@gmail.com>
2025-08-23 20:04:30 -0500

    Add LLNode

---
+++ CyForgeDS/base/LLNode.cpp
@@ -0,0 +1,15 @@
+template <typename T>
+
+class LLNode {
+	private:
+		T data;
+		LLNode* next;
+	public:
+		LLNode(const T& value): data(value), next(nullptr){}
+
+		T getData() const {return data;}
+		LLNode* getNext() const {return next;}
+
+		void setData(T value) {data = value;}
+		void setNext(LLNode* node) {next = node;} 
+};
