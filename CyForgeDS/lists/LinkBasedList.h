9683463bf283db03529e2b2fe4a796736c354009
rwiggins1 <wigginsricky94@gmail.com>
2025-08-27 11:41:31 -0500

    Add destructor to LinkedBasedList

--- CyForgeDS/lists/LinkBasedList.h
+++ CyForgeDS/lists/LinkBasedList.h
@@ -7,9 +7,23 @@ template <typename T>
 class LinkBasedList {
 private:
 	int element_num;
-	LLNode* front;
-	LLNode* rear;
+	LLNode<T>* front;
+	LLNode<T>* rear;
+
 public:
 	LinkBasedList() : element_num(0), front(nullptr), rear(nullptr) {}
-
+	
+	~LinkBasedList(){
+		clear();
+	}
+private:
+	void clear(){
+		while(front){
+			LLNode<T>* toDelete = front;
+			front = front->getNext();
+			delete toDelete;
+		}
+		rear = nullptr;
+		element_num = 0;
+	}
 };
---
+++ tests/cpp/LinkBasedList_test.cpp
@@ -0,0 +1,8 @@
+#include <cassert>
+#include "CyForgeDS/base/LLNode.h"
+#include "CyForgeDS/lists/LinkBasedList.h"
+
+int main(){
+	LinkBasedList<int> list;
+	return 0;
+}
